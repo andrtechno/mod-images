f12e671350eef0b9eeb10f373d9288a3bfab6de7
Andrew <andrew.panix@gmail.com>
2019-12-28 16:39:17 +0200

    init

--- migrations/m170622_111540_create_image_table.php
+++ migrations/m170622_111540_create_image_table.php
@@ -12,7 +12,10 @@ class m170622_111540_create_image_table extends Migration
     {
         $this->createTable(Image::tableName(), [
             'id' => $this->primaryKey(),
-            'filePath' => $this->string(400)->notNull(),
+            'filePath' => $this->string(255)->notNull(),
+            'path' => $this->string(255)->notNull(),
+            'handler_hash' => $this->string(8)->notNull(),
+            'handler_class' => $this->string(255)->notNull(),
             'object_id' => $this->integer(),
             'is_main' => $this->boolean()->defaultValue(0),
             'name' => $this->string(80),
@@ -24,6 +27,7 @@ class m170622_111540_create_image_table extends Migration
         $this->createIndex('ordern', Image::tableName(), 'ordern', 0);
         $this->createIndex('object_id', Image::tableName(), 'object_id', 0);
         $this->createIndex('is_main', Image::tableName(), 'is_main', 0);
+        $this->createIndex('handler_hash', Image::tableName(), 'is_main', 0);
     }
 
     public function down()
