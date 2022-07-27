1bdb46ca819a2474693ddcb04a8574027d928399
Andrew <andrew.panix@gmail.com>
2022-07-27 17:40:05 +0300

    add language uk,en

---
+++ messages/en/Image.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'ID' => 'ID',
+    'FILEPATH' => 'File path',
+    'OBJECT_ID' => 'Object',
+    'MODELNAME' => 'Model',
+    'URLALIAS' => 'Url Alias',
+    'IS_MAIN' => 'Cover',
+    'IMAGE' => 'Image',
+    'ALT_TITLE' => 'Alt+Title'
+];
---
+++ messages/uk/Image.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'ID' => 'ID',
+    'FILEPATH' => 'Шлях до файлу',
+    'OBJECT_ID' => 'Об\'єкт',
+    'MODELNAME' => 'Модель',
+    'URLALIAS' => 'Url Alias',
+    'IS_MAIN' => 'Обкладинка',
+    'IMAGE' => 'Зображення',
+    'ALT_TITLE' => 'Alt+Title'
+];
